ed00c4cae382211eebe0657841f1b77263841b0f
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-30 14:24:28 -0300

    Movimentação do Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     //Declarando o Rigdbody2D
     private Rigidbody2D meuRB;
+    [SerializeField] float velocidade = 5f;
 
     // Start is called before the first frame update
         void Start()
@@ -17,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Pegando o input Horizontal
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 minhaVelocidade = new Vector2(horizontal, vertical);
+        //Normalizando velocidade
+        minhaVelocidade.Normalize();
+        //Passando a minha velocidade para o meu RB
+        meuRB.velocity = minhaVelocidade * velocidade;
     }
 }
