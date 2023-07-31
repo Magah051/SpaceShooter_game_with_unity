8dd0f0af7b38434484b1627b056f37bb67fe88b7
José Magalhães <joses.magalhaes@hotmail.com>
2023-07-31 00:39:35 -0300

    Movendo o tiro para cima

---
+++ Assets/Scripts/TiroController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Declarando o Rigdbody2D
+    private Rigidbody2D meuRB;
+    [SerializeField] private float vel = 10f;
+    void Start()
+    {
+        //Pegando meu Rigdbody
+        meuRB = GetComponent<Rigidbody2D>();
+        //Indo para cima
+        meuRB.velocity = new Vector2(0f, vel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
